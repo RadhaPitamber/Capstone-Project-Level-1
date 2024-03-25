f050e4d62c18e02dc8731686adf6cfd1b730f8ac
RadhaPitamber <164070061+RadhaPitamber@users.noreply.github.com>
2024-03-25 19:15:36 +0200

    Add files via upload

---
+++ Python Code.py
@@ -0,0 +1,8 @@
+#Get user information
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+house_number = input("Enter your house number: ")
+street_name = input("Enter your street name: ")
+
+#Print out user details in a single sentence
+print(f" {name} is {age} years old, living at {house_number} on {street_name} street.")
